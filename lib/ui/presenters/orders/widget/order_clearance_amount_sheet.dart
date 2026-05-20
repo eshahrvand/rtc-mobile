2d44b7cc8b25cd03e8045f7307600a4dff774aac
mahdi ahbabi <mahdi.m12528@yahoo.com>
2026-05-20 11:18:08 +0330

    feat: enhance order clearance amount sheet layout and input handling

--- lib/ui/presenters/orders/widget/order_clearance_amount_sheet.dart
+++ lib/ui/presenters/orders/widget/order_clearance_amount_sheet.dart
@@ -20,110 +20,117 @@ class OrderClearanceAmountSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context).textTheme;
-    return Container(
-      padding: const EdgeInsets.fromLTRB(20, 10, 20, 32),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // Drag handle
-          Center(
-            child: Container(
-              width: 33,
-              height: 2,
-              decoration: BoxDecoration(color: AppColors.brandPalette.shade600),
-            ),
-          ),
-          const SizedBox(height: 40),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'مبلغ نهایی فاکتور:',
-                style: theme.bodyMedium!.copyWith(
-                  color: AppColors.grayPalette.shade700,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 32),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Drag handle
+            Center(
+              child: Container(
+                width: 33,
+                height: 2,
+                decoration: BoxDecoration(
+                  color: AppColors.brandPalette.shade600,
                 ),
               ),
-              Row(
-                children: [
-                  Text(
-                    totalAmount,
-                    style: theme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.grayPalette.shade900,
-                    ),
-                  ),
-                  const SizedBox(width: 4),
-                  Text(
-                    S.current.toman,
-                    style: theme.bodyLarge!.copyWith(
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.grayPalette.shade900,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 12),
-
-          const RtcDivider(isDashed: true),
-          const SizedBox(height: 12),
-
-          // Input label
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text(
-              'مبلغ تخلیه کارت را وارد کنید',
-              style: theme.bodyLarge!.copyWith(
-                fontWeight: FontWeight.w500,
-                color: AppColors.grayPalette.shade900,
-              ),
             ),
-          ),
-          const SizedBox(height: 12),
+            const SizedBox(height: 40),
 
-          Row(
-            spacing: 16,
-
-            children: [
-              Expanded(
-                child: RtcTextField(
-                  controller: amountController,
-                  textAlign: TextAlign.left,
-                  keyboardType: TextInputType.number,
-                  suffix: Padding(
-                    padding: const EdgeInsets.fromLTRB(14, 12, 0, 12),
-                    child: Text(
-                      S.current.toman,
-                      style: theme.bodySmall!.copyWith(
-                        color: AppColors.grayPalette.shade800,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'مبلغ نهایی فاکتور:',
+                  style: theme.bodyMedium!.copyWith(
+                    color: AppColors.grayPalette.shade700,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      totalAmount,
+                      style: theme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.grayPalette.shade900,
                       ),
                     ),
-                  ),
-                  textStyle: theme.bodyLarge!.copyWith(
-                    color: AppColors.grayPalette.shade900,
-                  ),
+                    const SizedBox(width: 4),
+                    Text(
+                      S.current.toman,
+                      style: theme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.grayPalette.shade900,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
+              ],
+            ),
+            const SizedBox(height: 12),
 
-              RtcButton(
-                title: 'بررسی',
-                onPressed: onCheckPressed,
-                height: 44,
-                width: 88,
-                styleBtn: theme.titleSmall!.copyWith(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
+            const RtcDivider(isDashed: true),
+            const SizedBox(height: 12),
+
+            // Input label
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'مبلغ تخلیه کارت را وارد کنید',
+                style: theme.bodyLarge!.copyWith(
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.grayPalette.shade900,
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 12),
+
+            Row(
+              spacing: 16,
+
+              children: [
+                Expanded(
+                  child: RtcTextField(
+                    controller: amountController,
+                    textAlign: TextAlign.left,
+                    keyboardType: TextInputType.number,
+                    suffix: Padding(
+                      padding: const EdgeInsets.fromLTRB(14, 12, 0, 12),
+                      child: Text(
+                        S.current.toman,
+                        style: theme.bodySmall!.copyWith(
+                          color: AppColors.grayPalette.shade800,
+                        ),
+                      ),
+                    ),
+                    textStyle: theme.bodyLarge!.copyWith(
+                      color: AppColors.grayPalette.shade900,
+                    ),
+                  ),
+                ),
+
+                RtcButton(
+                  title: 'بررسی',
+                  onPressed: onCheckPressed,
+                  height: 44,
+                  width: 88,
+                  styleBtn: theme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
