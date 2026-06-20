be13c39a81a76de6f9f8e2a402145dc827bf0357
mahdi ahbabi <mahdi.m12528@yahoo.com>
2026-06-20 18:15:27 +0330

    bump version to 1.0.0+18 for release preparation

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 String baseImage = "assets/images";
 
 String baseUrl =
-    // "https://api.rtciran.com/api/v1/";
-    "http://192.168.2.20:8001/api/v1/";
+    "https://api.rtciran.com/api/v1/";
+    // "http://192.168.2.20:8001/api/v1/";
